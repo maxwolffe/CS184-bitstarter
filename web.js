59b08a404f3ef3d0fd8c248b31769f20e5079e17
maxwolffe <max.alan.wolffe@gmail.com>
2013-07-13 06:00:47 +0000

    Got web.js to display index contents

--- web.js
+++ web.js
@@ -3,7 +3,8 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    var display = fs.readFileSync(index.html).toString();    
+    response.send(display);
 });
 
 var port = process.env.PORT || 5000;
